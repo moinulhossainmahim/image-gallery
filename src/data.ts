8f1c3c0b9ecfe0fabf8b9196ae2bf8b1202e89fd
mhossain2 <mhossain2@tibxtech.com>
2023-11-02 15:40:11 +0600

    initialized image gallery app

---
+++ src/data.ts
@@ -0,0 +1,51 @@
+export interface User {
+  id: number;
+  src: string | File;
+}
+
+export const data: User[] = [
+  {
+    id: 1,
+    src: "/images/image-11.jpeg",
+  },
+  {
+    id: 2,
+    src: "/images/image-2.webp",
+  },
+  {
+    id: 3,
+    src: "/images/image-3.webp",
+  },
+  {
+    id: 4,
+    src: "/images/image-4.webp",
+  },
+  {
+    id: 5,
+    src: "/images/image-5.webp",
+  },
+  {
+    id: 6,
+    src: "/images/image-6.webp",
+  },
+  {
+    id: 7,
+    src: "/images/image-7.webp",
+  },
+  {
+    id: 8,
+    src: "/images/image-8.webp",
+  },
+  {
+    id: 9,
+    src: "/images/image-9.webp",
+  },
+  {
+    id: 10,
+    src: "/images/image-10.jpeg",
+  },
+  {
+    id: 11,
+    src: "/images/image-1.webp",
+  },
+];
